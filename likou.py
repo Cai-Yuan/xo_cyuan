20818f75d178bb44275f548586b1647550503cde
cyuan <cyuan@clemson.edu>
2021-08-08 18:22:45 -0400

    build nodes

--- likou.py
+++ likou.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import numpy as np
 
+class node(object):
+    def __init__(self, val, hang,lie):
+        self.val=val
+        self.hang=hang
+        self.lie=lie
+        self.sun=[]
+    def add_sun(self,asunnode):
+        self.sun.append(asunnode)
+
 
 
 
